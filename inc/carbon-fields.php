cec8b7e96f3f94ef407bffe70353e0364b37a626
Kundius Ruslan <kundius.ruslan@gmail.com>
2025-12-16 21:29:48 +0000

    123

--- inc/carbon-fields.php
+++ inc/carbon-fields.php
@@ -4,6 +4,10 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 use Carbon_Fields\Block;
 
+add_action('after_setup_theme', function () {
+  \Carbon_Fields\Carbon_Fields::boot();
+});
+
 add_action('carbon_fields_register_fields', 'register_carbon_fields_blocks');
 function register_carbon_fields_blocks()
 {
