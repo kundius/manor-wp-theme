f82b45707eca2913d56dd14e835cdfe6c0ea678f
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-12-24 06:29:23 +0300

    upd

--- partials/feedback.php
+++ partials/feedback.php
@@ -1,7 +1,9 @@
 <section class="feedback">
   <div class="container">
-    <div class="feedback__title" data-scroll data-scroll-css-progress data-scroll-position="start, middle" data-scroll-offset="0, 40%">
-      Если вы хотите узнать стоимость индивидуальной разработки или уточнить цены по типовому проекту - свяжитесь с нами, менеджер предоставит бесплатную консультацию и даст ответы на вопросы
+    <div class="feedback__title-wrap">
+      <div class="feedback__title" data-scroll data-scroll-css-progress data-scroll-position="start, middle" data-scroll-offset="0, 40%">
+        Если вы хотите узнать стоимость индивидуальной разработки или уточнить цены по типовому проекту - свяжитесь с нами, менеджер предоставит бесплатную консультацию и даст ответы на вопросы
+      </div>
     </div>
     <div class="feedback__form">
       <?php get_template_part('blocks/feedback-form'); ?>
