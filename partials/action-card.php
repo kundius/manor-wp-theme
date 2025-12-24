1b53e9017a631d3ecc624d88391c826cfed92224
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-12-25 02:41:43 +0300

    upd

--- partials/action-card.php
+++ partials/action-card.php
@@ -2,15 +2,17 @@
   <figure class="action-card__media">
     <?php the_post_thumbnail('full', ['class' => 'action-card__image']); ?>
   </figure>
-  <div class="action-card__header">
-    <?php if ($result = getLastDayAndMonth()): ?>
-    <div class="action-card__date"><?php echo "Только до {$result['day']} {$result['month']}"; ?></div>
+  <div class="action-card__body">
+    <div class="action-card__header">
+      <?php if ($result = getLastDayAndMonth()): ?>
+      <div class="action-card__date"><?php echo "Только до {$result['day']} {$result['month']}"; ?></div>
+      <?php endif; ?>
+      <div class="action-card__title"><?php the_title(); ?></div>
+    </div>
+    <?php if ($content = get_the_content()): ?>
+    <div class="action-card__content">
+      <?php echo $content; ?>
+    </div>
     <?php endif; ?>
-    <div class="action-card__title"><?php the_title(); ?></div>
   </div>
-  <?php if ($content = get_the_content()): ?>
-  <div class="action-card__content">
-    <?php echo $content; ?>
-  </div>
-  <?php endif; ?>
 </article>
--- partials/article-card.php
+++ partials/article-card.php
@@ -2,16 +2,18 @@
   <figure class="article-card__media">
     <?php the_post_thumbnail('full', ['class' => 'article-card__image']); ?>
   </figure>
-  <div class="article-card__header">
-    <div class="article-card__date"><?php echo get_the_date('d.m.Y'); ?></div>
-    <div class="article-card__title"><?php the_title(); ?></div>
-  </div>
-  <?php if ($excerpt = get_the_excerpt()): ?>
-  <div class="article-card__content">
-    <?php echo $excerpt; ?>
-  </div>
-  <?php endif; ?>
-  <div class="article-card__more">
-    <a href="<?php the_permalink(); ?>" class="article-card__more-link">Читать далее</a>
+  <div class="article-card__body">
+    <div class="article-card__header">
+      <div class="article-card__date"><?php echo get_the_date('d.m.Y'); ?></div>
+      <div class="article-card__title"><?php the_title(); ?></div>
+    </div>
+    <?php if ($excerpt = get_the_excerpt()): ?>
+    <div class="article-card__content">
+      <?php echo $excerpt; ?>
+    </div>
+    <?php endif; ?>
+    <div class="article-card__more">
+      <a href="<?php the_permalink(); ?>" class="article-card__more-link">Читать далее</a>
+    </div>
   </div>
 </article>
